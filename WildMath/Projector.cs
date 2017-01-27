522b402726eb710373372b968e2a826ad7a61109
David Kaplan <dave@vyral.com>
2017-01-27 08:13:44 -0500

    Removed Projector class and streamlined PrimeProjector class

--- WildMath/Projector.cs
+++ WildMath/Projector.cs
@@ -6,36 +6,16 @@ using System.Threading.Tasks;
 
 namespace WildMath
 {
-	public class Projector
-	{
-		public virtual void ProjectVexel(Vexel vex)
-		{
-			throw new NotImplementedException();
-		}
-
-		public virtual void ProjectMaxel(Maxel max)
-		{
-			throw new NotImplementedException();
-		}
-	}
-
 	public class PrimeProjector
 	{
-		public PrimeProjector()
-		{
-			screen = 0;
-		}
-
-		public virtual void ProjectVexel(Vexel vex)
+		public static double Project(Vexel vex)
 		{
 			double val = 1;
 
 			foreach(KeyValuePair<int, int> kvp in vex.Elements)
 				val *= Math.Pow(kvp.Key, kvp.Value);
 
-			screen = val;
+			return val;
 		}
-
-		public double screen;
 	}
 }
